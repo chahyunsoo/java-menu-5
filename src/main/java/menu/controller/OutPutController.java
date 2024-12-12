abaa3dd3c3cedc66ce52380726bb4929381fe919
chahyunsoo <chahs1012@naver.com>
2024-12-12 18:38:45 +0900

    [feat] : 메인 로직 구현 완료 및 테스트 통과

--- src/main/java/menu/controller/OutPutController.java
+++ src/main/java/menu/controller/OutPutController.java
@@ -1,19 +1,25 @@
 package menu.controller;
 
 import java.util.List;
+import menu.model.Category;
 import menu.model.Coach;
+import menu.model.Menu;
 import menu.view.OutPutView;
 
 public class OutPutController {
     public final OutPutView outPutView;
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
+
     public OutPutController(OutPutView outPutView) {
         this.outPutView = outPutView;
     }
 
     public void run(List<Coach> coaches) {
-        
+        Category shuffledCategory = Category.pickCategoryByShuffle();
+        System.out.println("shuffledCategory.getCategoryName() = " + shuffledCategory.getCategoryName());
+        String pickedMenu = Menu.pickMenu(shuffledCategory.getCategoryName());
+        System.out.println("pickedMenu = " + pickedMenu);
     }
 
 
--- src/main/java/menu/model/Category.java
+++ src/main/java/menu/model/Category.java
@@ -1,11 +1,13 @@
 package menu.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public enum Category {
-    JAPAN("일식",1),
-    KOREAN("한식",2),
-    CHINA("중식",3),
-    ASIA("아시안",4),
-    WEST("양식",5);
+    JAPAN("일식", 1),
+    KOREAN("한식", 2),
+    CHINA("중식", 3),
+    ASIA("아시안", 4),
+    WEST("양식", 5);
 
     private final String categoryName;
     private final int categoryNumber;
@@ -21,5 +23,10 @@ public enum Category {
 
     public int getCategoryNumber() {
         return categoryNumber;
+    }
+
+    public static Category pickCategoryByShuffle() {
+        int pickNumberInRange = Randoms.pickNumberInRange(1, 5);
+        return Category.values()[pickNumberInRange];
     }
 }
